1d5024ac5d977950b951f478ab2e7dc08f32971f
youssef98123 <35854564+youssef98123@users.noreply.github.com>
2018-01-27 04:56:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,15 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log('I am ready!');
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag} !`);
-     client.user.setActivity("World",{type: 'WATCHING'});
-});
-
-client.login('token');
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
