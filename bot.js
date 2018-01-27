059511c6df3b4469acc56eed6de18903861b7c13
youssef98123 <35854564+youssef98123@users.noreply.github.com>
2018-01-27 05:03:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,6 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
 client.on('ready', () => {
-    console.log('I am ready!');
+        client.user.setActivity("Your Mom Moaing",{type: 'LISTENING'});
+
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
-
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
