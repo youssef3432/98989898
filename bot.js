54dc7b9e454e439725d870d18af3f94622d4fd9f
youssef98123 <35854564+youssef98123@users.noreply.github.com>
2018-01-27 06:02:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,50 @@
-client.on('ready', () => {
-        client.user.setActivity("Your Mom Moaing",{type: 'LISTENING'});
 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
 });
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+
+
+
+client.on('message', message => {
+if (message.content === "-server") {
+if(!message.channel.guild) return;
+const millis = new Date().getTime() - message.guild.createdAt.getTime();
+const now = new Date();
+
+const verificationLevels = ['None', 'Low', 'Medium', 'Insane', 'Extreme'];
+const days = millis / 1000 / 60 / 60 / 24;
+let roles = client.guilds.get(message.guild.id).roles.map(r => r.name);
+var embed  = new Discord.RichEmbed()
+.setAuthor(message.guild.name, message.guild.iconURL)
+.addField("**🆔 سيـرفر ايــدي**", "**"+message.guild.id+"**",true)
+.addField("**👑 سيــرفر اونـر**", "**"+message.guild.owner+"**" ,true)
+.addField("**✅ الشــات الاســاســي**" , "**"+message.guild.DefaultChannel+"**" ,true)
+.addField("**🌍 المـوقع**" , "**"+message.guild.region+"**",true)
+.addField('**💬 عدد الرومــات الكتابيــة**',`**[ ${message.guild.channels.filter(m => m.type === 'text').size} ] Channel **`,true)
+.addField("**📣 عدد الرومــات الصوتــية**", ` ** [ ${message.guild.channels.filter(m => m.type === 'voice').size} ] Channel ** `,true)
+.addField("**🤔 عدد ايــام انشــاء السيــرفر**", ` ** [ ${days.toFixed(0)} ] ** Day ` ,true)
+.addField("**👔 الــرتــب**",`**[${message.guild.roles.size}]** Role `,true)
+.addField("**💠 مســتوى حمــاية الســيرفر**", ` ** [ ${verificationLevels[message.guild.verificationLevel]} ] ** `,true)
+
+.addField("👥عدد الاعضــاء",`
+**${message.guild.memberCount}**`)
+.setThumbnail(message.guild.iconURL)
+.setColor('RANDOM')
+message.channel.sendEmbed(embed)
+
+}
+});
+
 
 client.login(process.env.BOT_TOKEN);
