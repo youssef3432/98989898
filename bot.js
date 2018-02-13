acf19d3fdf9b631f6ff74ee1e8e585314dc5e411
youssef98123 <35854564+youssef98123@users.noreply.github.com>
2018-02-14 02:47:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,20 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-var prefix = "*";
-
-client.on('message', message => {
-  if (!message.content.startsWith(prefix)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== '403568363516985345') return;
-
-if (message.content.startsWith(prefix + '1')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-if (message.content.startsWith(prefix + '2')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-if (message.content.startsWith(prefix + '3')) {
-client.user.setActivity(argresult, {type:'LISTENING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-if (message.content.startsWith(prefix + '4')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-}
+client.on('ready', function(){
+    client.user.setStatus("dnd");
+    var ms = 10000 ;
+    var setGame = [`محمد ${client.guilds.size} `,`محمد ${client.users.size}`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/KiNg66S`);
+    }, ms);10000
 
 });
-
 
 client.login(process.env.BOT_TOKEN);
