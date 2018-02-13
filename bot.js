84909764fd4fdf1418955dd40f7f84b602b9298d
youssef98123 <35854564+youssef98123@users.noreply.github.com>
2018-02-14 02:50:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,47 @@
-client.on('ready', function(){
-    client.user.setStatus("dnd");
-    var ms = 10000 ;
-    var setGame = [`محمد ${client.guilds.size} `,`محمد ${client.users.size}`];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`http://www.twitch.tv/KiNg66S`);
-    }, ms);10000
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(Logged in as ${client.user.tag}!);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'محمد') {
+    msg.reply('دقيقه');
+  }
+});
+
+
+client.on('message', message => {
+    var prefix = "-"
+    let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+
+if(command == "draw") {
+    var Canvas = require('canvas')
+  , Image = new Canvas.Image
+  , canvas = new Canvas(450, 170)
+  , ctx = canvas.getContext('2d');
+  ctx.font = '30px Impact';
+  let args = message.content.split(" ").slice(1);
+  
+Image.src = canvas.toBuffer();
+
+    console.log(Image);
+ctx.drawImage(Image, 0, 0, Image.width / 470, Image.height / 170);
+ctx.fillText(args.join("  "),110, 70);
+
+
+ctx.beginPath();
+ctx.lineTo(50, 102);
+ctx.stroke();
+
+message.channel.sendFile(canvas.toBuffer());
+}
+}).on('ready', () => {
 
 });
 
