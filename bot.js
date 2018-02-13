fabc4c3e8748550278f0e1cca210251789e52df2
youssef98123 <35854564+youssef98123@users.noreply.github.com>
2018-02-13 18:39:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,15 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const moment = require('moment');
+
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(Logged in as ${client.user.tag}!);
 });
 
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
 
 
           client.on('message', message => {
