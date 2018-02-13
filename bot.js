feea76729ca8e68953370c67fa2d2a6cca21654b
youssef98123 <35854564+youssef98123@users.noreply.github.com>
2018-02-13 18:31:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,59 +1,38 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const moment = require('moment');
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-client.on('message', message => {
-    if (message.content.startsWith("رابط")) {
-        
-  message.channel.createInvite({
-        thing: true,
-        maxUses: 1,
-        maxAge: 86400
-    }).then(invite =>  
-      message.author.sendMessage(invite.url)
-    )
-    const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setDescription("| :white_check_mark:  | :heart:  تم ارسال الرابط على الخاص  ")
-        .setFooter("Briolin")
-      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
-              const Embed11 = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setDescription(`
--------------------
-- تم إنشاء هذا الرابط لك
--  يمكن إستخدامه لشخص واحد فقط  
-- Briolin :heart: 
--------------------     `)
-        .setFooter("By:Briolin")
-      message.author.sendEmbed(Embed11)
-    }
-});
-
+          client.on('message', message => {
+           if (message.content.startsWith("الأمر")) {
+     var args = message.content.split(" ").slice(1);
+     let user = message.mentions.users.first();
+     var men = message.mentions.users.first();
+        var heg;
+        if(men) {
+            heg = men
+        } else {
+            heg = message.author
+        }
+      var mentionned = message.mentions.members.first();
+         var h;
+        if(mentionned) {
+            h = mentionned
+        } else {
+            h = message.member
+        }
+               moment.locale('ar-TN');
+      var id = new  Discord.RichEmbed()
+    .setColor("RANDOM")
+    .addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)
+    .addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true)
+   .setThumbnail(message.author.avatarURL)
+    message.channel.send(id)
+}       });
 
 
 client.login(process.env.BOT_TOKEN);
