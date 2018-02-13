d33d0ce4eaa094e6c43e634c37ddbb0a7023638b
youssef98123 <35854564+youssef98123@users.noreply.github.com>
2018-02-13 18:40:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,12 +5,6 @@ client.on('ready', () => {
   console.log(Logged in as ${client.user.tag}!);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
 
           client.on('message', message => {
            if (message.content.startsWith("id")) {
