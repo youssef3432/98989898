92b80f971910c02dbc9dfa1349a63d01dea30f5e
youssef98123 <35854564+youssef98123@users.noreply.github.com>
2018-03-04 13:27:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,30 +4,30 @@ const client = new Discord.Client();
 console.log("Welcome Again !");
 
 client.on('ready', () => {
-    client.user.setStatus('online');
+    client.user.setStatus('idle');
 });
 
 client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
-  return channel.send(**
+  return channel.send(`**
       حياك الله ي بعد راسي
        خش بتنورنا فعاليات وكل شيء حلو موجود !
         ي بعد عيني الرابط تحت
-         
+          (:
 
-                                 [  https://discord.gg/zJeF3kY ] **)
+                                 [ https://discord.gg/zJeF3kY ] **`)
 }).catch(console.error)
 })
 
 client.on("guildMemberRemove", member => {
   member.createDM().then(function (channel) {
-  return channel.send(**
+  return channel.send(`**
       حياك الله ي بعد راسي
        خش بتنورنا فعاليات وكل شيء حلو موجود !
         ي بعد عيني الرابط تحت
-         
+          (:
 
-                                 [  https://discord.gg/zJeF3kY ] **)
+                                 [ https://discord.gg/zJeF3kY ] **`)
 }).catch(console.error)
 })
 
